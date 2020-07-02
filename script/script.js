f9f14f58ae3bd17efa59f5705138e5643c26ee92
Shamil <Shamilllaa@mail.ru>
2020-07-02 20:10:32 +0500

    'lesson21_rer'

--- script/script.js
+++ script/script.js
@@ -57,7 +57,7 @@ window.addEventListener('DOMContentLoaded', function(){
 		};
 		document.body.addEventListener('click', (event) => {
 			let target = event.target;
-			if(target.closest('.menu') || target.closest('.close-btn') || target.closest('menu ul>li') || 
+			if(target.closest('.menu') || target.closest('.close-btn') || (target.closest('menu ul>li>a')) || 
 				(!target.closest('.active-menu') && document.querySelector('.active-menu'))) {
 					handlerMenu();
 			}
